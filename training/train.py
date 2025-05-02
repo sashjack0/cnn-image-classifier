5580d5d8629ffa3417b9807bf00727f148737edb
sashjack0 <sashjack@gmail.com>
2025-05-02 13:44:29 +0530

    Add __init__.py files and ignore dataset folder

--- training/train.py
+++ training/train.py
@@ -1,6 +1,9 @@
 # training/train.py
 
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
